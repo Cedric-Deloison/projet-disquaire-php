6c07000c1b223b1978adc55909cba219099f87ec
Cédric DELOISON <cedric.deloison@icloud.com>
2020-10-22 14:04:54 +0200

    ajouts fonct

--- 4disquaire.php
+++ 4disquaire.php
@@ -4,29 +4,31 @@
 
 //$tab = array(1, 2, 3, 4);
 //echo somme($tab);
-  
-    if (isset($_GET['id'])){
-        $id = $_GET['id'];
-    }else{
-        $id = 0;
-    }
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+} else {
+    $id = 0;
+}
 
 //    if(testId($id)){
-        $sql =  'SELECT album, alimage FROM albums ORDER BY album';
-        foreach  ($dbh->query($sql) as $row) {
-                $album = $row['album'];                
-                $titre = $row['titre'];
-                $année = $row['année'];
-                $genre = $row['genre'];
-                $photo = $row['alimage'];
+$sql =  'SELECT titre, alimage, annee, album, N.styles, genre, M.noms 
+FROM albums U, genres N, artistes M
+WHERE N.id = U.genre AND M.id = U.artiste 
+ORDER BY album';
+foreach ($dbh->query($sql) as $row) {
+    $album = $row['album'];
+    $titre = $row['titre'];
+    $annee = $row['annee'];
+    $genre = $row['styles'];
+    $photo = $row['alimage'];
+    $artiste = $row['noms'];
 
 
-                echo "<div class='row'>";
-                affich($photo, $titre);
-                echo "</div>";
-
-
-        }
+    echo "<div class='row'>";
+    affich($photo, $titre, $album, $annee, $genre, $artiste);
+    echo "</div>";
+}
 
 //    }
 
