8b8f5e002244fca2de84078992a95e060f5c710c
Cédric DELOISON <cedric.deloison@icloud.com>
2020-10-25 15:41:08 +0100

    ajouts accueil

--- router/home.php
+++ router/home.php
@@ -3,5 +3,15 @@
 
 $texte = array(
     "titre" => "Bienvenue", "sous-titre" => "Un site dynamique",
-    "contenu" => "Construction pas à pas d'un site dynamique"
+    "contenu" => '<div class="jumbotron">
+    <h1 class="display-4">Bienvenue sur notre boutique de disques!</h1>
+    <p class="lead">Une petite présentation de notre boutique : </p>
+    <hr class="my-4">
+    <p>C’est en 1886 que Joseph Gibert, professeur de Lettres Classiques au Collège Saint-Michel à Saint-Étienne, décide de « monter » à Paris où il ouvre, face à Notre Dame, sur le parapet du quai Saint-Michel, quatre boîtes de bouquiniste.
+
+    Dès 1888, il inaugure sa première librairie 23 quai Saint-Michel, spécialisée dans le négoce du livre scolaire d’occasion. Grâce à Jules Ferry qui rend l’école obligatoire, la librairie connaît un franc succès. Le nom GIBERT devient un acteur connu et reconnu du livre sur la place parisienne.
+    
+    Après quarante ans d’exploitation sur la rive gauche, la librairie GIBERT connaît sa première évolution. En 1929, la société GIBERT est scindée en deux. Le fondateur, décédé en 1915, laisse deux fils. Le premier s’installe sous son nom GIBERT JOSEPH dans le 6e arrondissement au 30 boulevard Saint-Michel. Son frère cadet continue l’exploitation quai St Michel sous l’enseigne GIBERT JEUNE. </p>
+    <img src="template/bateau" alt="..." >
+  </div>',
 );
